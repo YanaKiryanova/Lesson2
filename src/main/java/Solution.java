7ca15f1af88d0e55063915aecf71fefc59cef915
YanaKiryanova <yan573@yandex.ru>
2022-09-22 22:08:30 +0300

    Homework

---
+++ src/main/java/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        ///Task 1
+        System.out.println("Hello World");
+        //Task 2
+        Scanner sc = new Scanner(System.in);
+        String user = null;
+        if (sc.hasNextLine()){
+            user = sc.nextLine();
+        }
+        System.out.println("Hello " + user );
+        sc.close();
+    }
+}
