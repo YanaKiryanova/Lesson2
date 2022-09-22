e3a03da9ed7e382a726acf79fa797c4f3c4e4610
YanaKiryanova <yan573@yandex.ru>
2022-09-22 22:25:59 +0300

    Fix str

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -5,6 +5,7 @@ public class Solution {
         ///Task 1
         System.out.println("Hello World");
         //Task 2
+        System.out.println("Введите своё имя");
         Scanner sc = new Scanner(System.in);
         String user = null;
         if (sc.hasNextLine()){
